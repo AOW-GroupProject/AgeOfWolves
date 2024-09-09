b4a9d085b5ff64cf275663ccac3be6c7bdb12553
서범준 / BeomJun Seo <88536510+Seo-BJ@users.noreply.github.com>
2024-09-09 22:01:44 +0900

    Feat : 선입력 시스템 (#59) (#65)
    * Feat : 선입력 시스템 (#59)
    
    * Sync: Develop 브랜치와 Conflict 발생 및 Conflict 수정 작업
    
    * Sync: Conflict 해결
    
    ---------
    
    Co-authored-by: unknown <hardii2unreal@gmail.com>

---
+++ Source/AgeOfWolves/04_Component/PlayerAbilitySystemComponent.cpp
@@ -0,0 +1,120 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "04_Component/PlayerAbilitySystemComponent.h"
+
+#include "GameplayTagContainer.h"
+#include "02_AbilitySystem/AOWGameplayTags.h"
+
+
+
+
+void UPlayerAbilitySystemComponent::InitializeComponent()
+{
+	Super::InitializeComponent();
+	AbilityFailedCallbacks.AddUObject(this, &UPlayerAbilitySystemComponent::SaveBlockedAbility);
+}
+
+void UPlayerAbilitySystemComponent::OnAbilityEnded(UGameplayAbility* Ability)
+{
+	Super::OnAbilityEnded(Ability);
+
+	// Passive Ability는 선입력과 관계없기 때문에 조기 Retrun
+	const FGameplayTag PassiveTag = FGameplayTag::RequestGameplayTag(FName(TEXT("Ability.Passive")));
+	if (Ability->AbilityTags.HasTag(PassiveTag))
+	{
+		return;
+	}
+	// 저장된 어빌리티가 있다면 실행한다.
+	if (SavedAbility)
+	{
+		TryActivateAbility(SavedAbility->GetCurrentAbilitySpecHandle());
+		SavedAbility = nullptr;
+	}
+}
+
+void UPlayerAbilitySystemComponent::SaveBlockedAbility(const UGameplayAbility* BlockedAbility, const FGameplayTagContainer& TagContainer)
+{
+	if (bCanSaveBlockedAbility == false)
+	{
+		return;
+	}
+
+	// Passive Ability는 선입력과 관계없기 때문에 조기 Return
+	const FGameplayTag PassiveTag = FGameplayTag::RequestGameplayTag(FName(TEXT("Ability.Passive")));
+	if (BlockedAbility->AbilityTags.HasTag(PassiveTag))
+	{
+		return;
+	}
+	FGameplayTagContainer CurrentOwnedGameplayTags;
+	GetOwnedGameplayTags(CurrentOwnedGameplayTags);
+
+	// 공격 어빌리티 실행 중 닷지 어빌리티 실행 시 저장
+	const FGameplayTag NormalAttackascendantTag = FGameplayTag::RequestGameplayTag(FName(TEXT("Ability.Active.Attack")));
+	if (CurrentOwnedGameplayTags.HasTag(NormalAttackascendantTag))
+	{
+		if (BlockedAbility->AbilityTags.HasTag(AOWGameplayTags::Ability_Active_Dodge))
+		{
+			SavedAbility = BlockedAbility;
+			return;
+		}
+	}
+	
+	// 좌클릭 입력시 다음으로 올 수 있는 능력만 저장 
+	if (CurrentOwnedGameplayTags.HasTagExact(AOWGameplayTags::Ability_Ready_Attack_Normal_2))
+	{
+		if (BlockedAbility->AbilityTags.HasTagExact(AOWGameplayTags::Ability_Active_Attack_Normal_2))
+		{
+			SavedAbility = BlockedAbility;
+		}
+		return;
+	}
+	else if (CurrentOwnedGameplayTags.HasTagExact(AOWGameplayTags::Ability_Ready_Attack_Normal_3))
+	{
+		if (BlockedAbility->AbilityTags.HasTagExact(AOWGameplayTags::Ability_Active_Attack_Normal_3))
+		{
+			SavedAbility = BlockedAbility;
+		}		
+		return;
+	}
+	else if (CurrentOwnedGameplayTags.HasTagExact(AOWGameplayTags::Ability_Ready_Attack_Normal_4))
+	{
+		if (BlockedAbility->AbilityTags.HasTagExact(AOWGameplayTags::Ability_Active_Attack_Normal_4))
+		{
+			SavedAbility = BlockedAbility;
+		}
+		return;
+	}
+	else if (CurrentOwnedGameplayTags.HasTagExact(AOWGameplayTags::Ability_Ready_Attack_Normal_4))
+	{
+		if (BlockedAbility->AbilityTags.HasTagExact(AOWGameplayTags::Ability_Active_Attack_Normal_4))
+		{
+			SavedAbility = BlockedAbility;
+		}
+		return;
+	}
+	else if (CurrentOwnedGameplayTags.HasTagExact(AOWGameplayTags::Ability_Ready_Attack_DodgeAttack))
+	{
+		if (BlockedAbility->AbilityTags.HasTagExact(AOWGameplayTags::Ability_Active_Attack_DodgeAttack))
+		{
+			SavedAbility = BlockedAbility;
+		}
+		return;
+	}
+	SavedAbility = BlockedAbility;
+}
+void UPlayerAbilitySystemComponent::EarlyCancelAbility()
+{
+	// 저장된 어빌리티가 있으면, 현재 실행중인 어빌리티를 찾아 종료함.
+	if (SavedAbility)
+	{
+		FGameplayTagContainer CurrentOwnedGameplayTags;
+		GetOwnedGameplayTags(CurrentOwnedGameplayTags);
+		CancelAbilities(&CurrentOwnedGameplayTags);
+	}
+}
+void UPlayerAbilitySystemComponent::SetbCanSaveBlockedAbility(bool bCanSave)
+{
+	bCanSaveBlockedAbility = bCanSave;
+
+}
---
+++ Source/AgeOfWolves/04_Component/PlayerAbilitySystemComponent.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "04_Component/BaseAbilitySystemComponent.h"
+
+#include "PlayerAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGEOFWOLVES_API UPlayerAbilitySystemComponent : public UBaseAbilitySystemComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	// @목적 : 현재 실행중인 몽타주 및 어빌리티를 조기 종료하는 함수
+	UFUNCTION(BlueprintCallable)
+	void EarlyCancelAbility();
+
+	// @목적 : bCanSaveBlockedAbility 멤버 변수 Setter
+	UFUNCTION(BlueprintCallable)
+	void SetbCanSaveBlockedAbility(bool bCanSave);
+	// @목적 : bCanSaveBlockedAbility 멤버 변수 Getter
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE bool GetbCanSaveBlockedAbility(){ return bCanSaveBlockedAbility; }
+
+protected:
+
+	virtual void InitializeComponent() override;
+	virtual void OnAbilityEnded(UGameplayAbility* Ability) override;
+
+private:
+
+	bool bCanSaveBlockedAbility = false;
+
+	// @목적 : 선입력 된 어빌리티를 저장하는 변수
+	const UGameplayAbility* SavedAbility = nullptr;
+	// @목적 : 선입력 된 어빌리티를 저장하는 함수
+	void SaveBlockedAbility(const UGameplayAbility* BlockedAbility, const FGameplayTagContainer& TagContainer);
+};
