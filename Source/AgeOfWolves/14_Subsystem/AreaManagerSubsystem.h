538aa6f88c79b168cc98c9fd2939c70d7e203700
unknown <hardii2unreal@gmail.com>
2025-04-23 22:52:30 +0900

    Feature: AI 비전투 행동 방식
    Patrol Path를 따라서 AI가 이동, 비전투시 행동 방식 완료
    
    resolves #200
    related to -
    ref to -

---
+++ Source/AgeOfWolves/14_Subsystem/AreaManagerSubsystem.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+
+#include "AreaManagerSubsystem.generated.h"
+
+//@전방 선언
+#pragma region Forward Declaration
+class AArea;
+struct FAIGroupInfo;
+#pragma endregion
+
+//@열거형
+#pragma region Enums
+#pragma endregion
+
+//@구조체
+#pragma region Structs
+#pragma endregion
+
+//@이벤트/델리게이트
+#pragma region Delegates
+#pragma endregion
+
+/**
+ * @UAreaManagerSubsystem
+ *
+ * 월드에 배치된 Area들을 관리하고 필요한 정보를 제공하는 서브시스템
+ */
+UCLASS()
+class AGEOFWOLVES_API UAreaManagerSubsystem : public UGameInstanceSubsystem
+{
+//@친추 클래스
+#pragma region Friend Class
+#pragma endregion
+
+    GENERATED_BODY()
+
+//@Defualt Setting
+#pragma region Default Setting
+public:
+    //@초기화
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+    virtual void Deinitialize() override;
+#pragma endregion
+
+//@Property/Info...etc
+#pragma region Property or Subwidgets or Infos...etc
+public:
+    //@Area 등록/해제
+    UFUNCTION(BlueprintCallable, Category = "Area|Management")
+    void RegisterArea(AArea* Area);
+
+    UFUNCTION(BlueprintCallable, Category = "Area|Management")
+    void UnregisterArea(AArea* Area);
+
+private:
+    //@등록된 Area 맵
+    UPROPERTY()
+    TMap<FGuid, AArea*> RegisteredAreas;
+#pragma endregion
+
+//@Delegates
+#pragma region Delegates
+#pragma endregion
+
+//@Callbacks
+#pragma region Callbacks
+#pragma endregion
+
+//@Utility(Setter, Getter,...etc)
+#pragma region Utility
+public:
+    //@Area 검색
+    UFUNCTION(BlueprintCallable, Category = "Area|Management")
+    AArea* GetAreaByID(const FGuid& AreaID) const;
+
+    //@AI 관련 정보 제공
+    UFUNCTION(BlueprintCallable, Category = "Area|AI")
+    FGuid GetAIGroupID(AActor* AIActor) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Area|AI")
+    TArray<FAIGroupInfo> GetAllGroups() const;
+
+    UFUNCTION(BlueprintCallable, Category = "Area|AI")
+    bool GetGroupInfo(const FGuid& GroupID, FAIGroupInfo& OutGroupInfo) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Area|AI")
+    AActor* GetPatrolPathForGroup(const FGuid& GroupID) const;
+#pragma endregion
+
+};
