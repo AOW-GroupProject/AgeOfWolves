ac3ed08c7996f6a26b899cd369eed9a2de8bf4c7
unknown <hardii2unreal@gmail.com>
2025-05-20 19:05:49 +0900

    feature: 전투 개선
    Attack 유형의 어빌리티 구성 변경 및 충돌 연출 관련 속성 변경, 기본 공격의 Collision FX 구성 테스트 완료
    
    resolves #236
    related to -
    ref to -

--- Stats/Source/AgeOfWolves/01_Character/TeamSettingInfos.cpp
+++
@@ -1,8 +0,0 @@
-#include "TeamSettingInfos.h"
-#include "Logging/StructuredLog.h"
-
-DEFINE_LOG_CATEGORY(LogTeamSettingInfos)
-
-UTeamSettingInfos::UTeamSettingInfos(const FObjectInitializer& ObjectInitializer)
-	:Super(ObjectInitializer)
-{}
--- Stats/Source/AgeOfWolves/10_AI/AIDataSetInfos.cpp
+++
@@ -1,8 +0,0 @@
-#include "AIDataSetInfos.h"
-#include "Logging/StructuredLog.h"
-
-DEFINE_LOG_CATEGORY(LogAIDataSetInfos)
-
-UAIDataSetInfos::UAIDataSetInfos(const FObjectInitializer& ObjectInitializer)
-	:Super(ObjectInitializer)
-{}
